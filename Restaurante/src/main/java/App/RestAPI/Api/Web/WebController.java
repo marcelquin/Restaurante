93e0c2e4c62a4227446cdea462e1d2342cf9ebb3
Marcel Higor quinteiro Araujo <117388829+marcelquin@users.noreply.github.com>
2024-03-25 10:52:49 -0400

    thymeleaf adicionado
    estrutura básica de pagina ok

--- Restaurante/src/main/java/App/RestAPI/Api/Web/WebController.java
+++ Restaurante/src/main/java/App/RestAPI/Api/Web/WebController.java
@@ -1,12 +1,18 @@
 package App.RestAPI.Api.Web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class WebController {
 
-    ModelAndView vs = new ModelAndView();
+    @RequestMapping("/")
+    public ModelAndView index()
+    {
+        ModelAndView mv = new ModelAndView("index");
+        return mv;
+    }
 
 
 }
